2f07824394603b107b177dfdeb202f2d3ee6f79b
Miguel Angel <elmendalerenda@gmail.com>
2013-11-02 10:38:18 +0000

    added description to the company

--- lib/company.rb
+++ lib/company.rb
@@ -1,2 +1,2 @@
-class Company < Struct.new(:name, :address, :website, :logo, :email, :keywords)
+class Company < Struct.new(:name, :address, :website, :logo, :email, :keywords, :description)
 end
--- test/fixtures/company.rb
+++ test/fixtures/company.rb
@@ -8,5 +8,5 @@ database_settings = MyApp.settings.database
 @database = MongoClient.new(database_settings["host"], database_settings["port"]).db(database_settings["name"])
 @companies_collection = @database.collection("companies")
 @companies_collection.insert("name" => "Aureka", "hiring" => true, "keywords" => ['tag1', 'tag2']);
-@companies_collection.insert("name" => "startup molona", "hiring" => true, "keywords" => ['tag1', 'tag2']);
-@companies_collection.insert("name" => "Vetaminic", "hiring" => false, "keywords" => ['tag1', 'tag2']);
+@companies_collection.insert("name" => "startup molona", "description" => "los animalicos", "hiring" => true, "keywords" => ['tag1', 'tag2']);
+@companies_collection.insert("name" => "Vetaminic", "address" => "patatal numero 3", "hiring" => false, "keywords" => ['tag1', 'tag2']);
